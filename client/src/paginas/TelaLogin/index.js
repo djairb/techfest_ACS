8271b4aecf766b985df0465c70a5873855663da2
djairb <djair.batista@outlook.com>
2024-04-02 08:49:07 -0300

    não vamos usar back end aqui

--- client/src/paginas/TelaLogin/index.js
+++ client/src/paginas/TelaLogin/index.js
@@ -15,7 +15,7 @@ import Footer from '../../componentes/Footer/Footer';
 
 function TelaLogin() {
 
-  const {user, toggleUser} = useContext(UserContext);
+  const { user, toggleUser } = useContext(UserContext);
 
   const userRecusado = {
 
@@ -57,7 +57,7 @@ function TelaLogin() {
   // useEffect é utilizado com a dependência [logado]. Isso significa que o bloco de código dentro do useEffect será executado sempre que o valor de logado for alterado. Isso permite que você realize ações adicionais sempre que o estado logado for modificado.
 
   // };
-  
+
 
   useEffect(() => {
     // O código dentro deste bloco será executado sempre que 'logado' for alterado
@@ -71,12 +71,14 @@ function TelaLogin() {
 
     }
 
-    if (logado){
+    if (logado) {
 
       userAceito.texto = `Bem vindo usuário com ID: ${dadoPadrao.id}`
       //cuidado na parte deslogar !!!!!!!!!!!
 
       toggleUser(dadoPadrao);
+      navigate('/');
+
     }
 
 
@@ -85,18 +87,19 @@ function TelaLogin() {
   const handleClickButton = async () => {
     const response = await Axios.get("http://localhost:3001/getUser", { params: { usuario: values.usuario, senha: values.senha } });
     let numero = response.data.length;
-    
+
     //os valores sao alterados aqui. mas sem o useEffect voce nao pode usar fora da funcao. mas o useeffect roda um conjunto de funcoes de acordo com as modificações, feitas internas ou nao - como é que tá? Jesus é bom.
     { (numero > 0) ? setLogado(true) : setLogado(false) }
 
-    if(numero > 0){
+    if (numero > 0) {
       //setei dadoPadrao e coloquei ele como dependencia no effect. ele não modifica a menos que coloque como dependencia no effect e assim faça de fato o valor ser setado.
       setDadoPadrao(response.data[0])
     }
-    
+
   };
 
-  const navegarPagina = () =>{
+
+  const navegarPagina = () => {
 
     navigate('/pagina-crud', { state: dadoPadrao });
 
@@ -107,10 +110,10 @@ function TelaLogin() {
 
 
     <>
-      
+
 
       <div className='app-container'>
-        <Navbar/>
+        <Navbar />
         <div className='register--container'>
 
           <h1 className='login-name'>LOGIN</h1>
@@ -143,23 +146,23 @@ function TelaLogin() {
 
           <h1 className={texto.style}>{texto.texto}</h1>
 
-          
+
 
         </div>
 
 
-        <Footer/>
+        <Footer />
 
       </div>
 
-      
-      
-    
-    
-    
-    
+
+
+
+
+
+
     </>
-    
+
 
   );
 }
