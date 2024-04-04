375141f81bf55fe41025b38ef4ee3996a8d1f202
djairb <djair.batista@outlook.com>
2024-04-04 09:28:30 -0300

    card css finalizado (?)

--- client/src/data/dados.js
+++ client/src/data/dados.js
@@ -2,7 +2,7 @@ export const standList = [
 
   {
     id: 1,
-    imgSrc: "https://onedrive.live.com/embed?resid=3282EDF418D3DB36%211527&authkey=%21AB2j_j3m3YJ-Dj8",
+    imgSrc: "https://onedrive.live.com/embed?resid=3282EDF418D3DB36%212195&authkey=%21ACxUYkqVEVg0KIk",
     titulo: "Arena Esports",
     subtitulo: "Projeto Passaporte Digital",
     descricao: "Neste stand acontecerá a Liga Gamer de Futebol - FIFA, com inscrições abertas ao público. A iniciativa está sendo organizada por jovens do Projeto Passaporte Digital."
@@ -10,7 +10,7 @@ export const standList = [
   },
   {
     id: 2,
-    imgSrc: "https://onedrive.live.com/embed?resid=3282EDF418D3DB36%211527&authkey=%21AB2j_j3m3YJ-Dj8",
+    imgSrc: "https://onedrive.live.com/embed?resid=3282EDF418D3DB36%212195&authkey=%21ACxUYkqVEVg0KIk",
     titulo: "Arena Esports",
     subtitulo: "Projeto Passaporte Digital",
     descricao: "Neste stand acontecerá a Liga Gamer de Futebol - FIFA, com inscrições abertas ao público. A iniciativa está sendo organizada por jovens do Projeto Passaporte Digital."
@@ -18,7 +18,7 @@ export const standList = [
   },
   {
     id: 3,
-    imgSrc: "https://onedrive.live.com/embed?resid=3282EDF418D3DB36%211527&authkey=%21AB2j_j3m3YJ-Dj8",
+    imgSrc: "https://onedrive.live.com/embed?resid=3282EDF418D3DB36%212195&authkey=%21ACxUYkqVEVg0KIk",
     titulo: "Arena Esports",
     subtitulo: "Projeto Passaporte Digital",
     descricao: "Neste stand acontecerá a Liga Gamer de Futebol - FIFA, com inscrições abertas ao público. A iniciativa está sendo organizada por jovens do Projeto Passaporte Digital."
@@ -26,11 +26,29 @@ export const standList = [
   },
   {
     id: 4,
-    imgSrc: "https://onedrive.live.com/embed?resid=3282EDF418D3DB36%211527&authkey=%21AB2j_j3m3YJ-Dj8",
+    imgSrc: "https://onedrive.live.com/embed?resid=3282EDF418D3DB36%212195&authkey=%21ACxUYkqVEVg0KIk",
     titulo: "Arena Esports",
     subtitulo: "Projeto Passaporte Digital",
     descricao: "Neste stand acontecerá a Liga Gamer de Futebol - FIFA, com inscrições abertas ao público. A iniciativa está sendo organizada por jovens do Projeto Passaporte Digital."
 
   },
+  {
+    id: 4,
+    imgSrc: "https://onedrive.live.com/embed?resid=3282EDF418D3DB36%212195&authkey=%21ACxUYkqVEVg0KIk",
+    titulo: "Arena Esports",
+    subtitulo: "Projeto Passaporte Digital",
+    descricao: "Neste stand acontecerá a Liga Gamer de Futebol - FIFA, com inscrições abertas ao público. A iniciativa está sendo organizada por jovens do Projeto Passaporte Digital."
+
+  },
+  {
+    id: 4,
+    imgSrc: "https://onedrive.live.com/embed?resid=3282EDF418D3DB36%212195&authkey=%21ACxUYkqVEVg0KIk",
+    titulo: "Arena Esports",
+    subtitulo: "Projeto Passaporte Digital",
+    descricao: "Neste stand acontecerá a Liga Gamer de Futebol - FIFA, com inscrições abertas ao público. A iniciativa está sendo organizada por jovens do Projeto Passaporte Digital."
+
+  },
+
+
 
 ];
